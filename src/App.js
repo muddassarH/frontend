339325651ddd4f76e2dbbe06814fb14d13cf71ad
muddassarH <muddassarhussain90@gmail.com>
2025-05-24 13:17:34 +0500

    ENV CHANGES..

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
   const [data, setData] = useState(null);
-  const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api/hello/'; 
+  const BASE_URL = process.env.REACT_APP_API_URL
+  const API_URL = `${BASE_URL}/api/hello/`;
+  // const API_URL = 'http://localhost:8000/api/hello';
   // ^ If REACT_APP_API_URL is not set, default to a local Django server for dev.
 
   useEffect(() => {
